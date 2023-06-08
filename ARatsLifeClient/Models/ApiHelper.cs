a501c8652aaad38c34bf3c5ea1cde9c2c2925657
Kyle Crawford <kcraw217@gmail.com>
2023-06-08 08:48:23 -0700

    add error handling to register

--- ARatsLifeClient/Models/ApiHelper.cs
+++ ARatsLifeClient/Models/ApiHelper.cs
@@ -1,21 +1,24 @@
 using System.Threading.Tasks;
 using RestSharp;
+using ARatsLifeClient.ViewModels;
 
 namespace ARatsLifeClient.Models
 {
   public class ApiHelper
   {
+
+    private const string HOSTNAME = "http://localhost:5102/";
     public static async Task<string> GetAllRats()
     {
-      RestClient client = new RestClient("http://localhost:5102/");
-      RestRequest request = new RestRequest($"api/rats", Method.Get);
-      RestResponse response = await client.GetAsync(request);
+      var client = new RestClient(HOSTNAME);
+      var request = new RestRequest($"api/rats", Method.Get);
+      var response = await client.GetAsync(request);
       return response.Content;
     }
 
     public static async Task<string> GetRat(int id)
     {
-      RestClient client = new RestClient("http://localhost:5102/");
+      RestClient client = new RestClient(HOSTNAME);
       RestRequest request = new RestRequest($"api/rats/{id}", Method.Get);
       RestResponse response = await client.GetAsync(request);
       return response.Content;
@@ -23,17 +26,31 @@ namespace ARatsLifeClient.Models
     //--------------------------- 
     // for application users
 
-    public static async void Register(string newApplicationUser)
+    public static async Task RegisterAsync(RegisterViewModel newApplicationUser)
     {
-      RestClient client = new RestClient("http://localhost:5102/");
-      RestRequest request = new RestRequest($"api/accounts", Method.Post);
-      request.AddHeader("Content-Type", "application/json");
-      request.AddJsonBody(newApplicationUser);
-      await client.PostAsync(request);
+      using var httpClient = new HttpClient();
+      var path = HOSTNAME + "api/accounts";
+      var resp = await httpClient.PostAsJsonAsync<RegisterViewModel>(path, newApplicationUser);
+
+      if (resp.IsSuccessStatusCode) {
+        // return resp.Content
+        return;
+      }
+
+      var returnedMsg = await resp.Content.ReadAsStringAsync();
+      throw new KylesCustomException(returnedMsg);
+      // var client = new RestClient(HOSTNAME);
+      // var request = new RestRequest($"api/accounts", Method.Post);
+      // request.AddHeader("Content-Type", "application/json");
+      // request.AddJsonBody(newApplicationUser);
+
+      // await client.PostAsync(request);
+      // Console.WriteLine("Hummus");
     }
+
     public static async Task<string> Login(string userInfo)
     {
-      RestClient client = new RestClient("http://localhost:5102/");
+      RestClient client = new RestClient(HOSTNAME);
       RestRequest request = new RestRequest($"api/accounts/login", Method.Post);
       request.AddHeader("Content-Type", "application/json");
       request.AddJsonBody(userInfo);
